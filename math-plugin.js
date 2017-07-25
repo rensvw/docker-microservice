00f3f2f859cf41b326b1dc2dbb61c27d7c57d688
Rens van Woudenberg <rensvanw@outlook.com>
2017-07-25 16:09:53 +0200

    Fixed error in sum function

--- math-plugin.js
+++ math-plugin.js
@@ -3,7 +3,7 @@
 module.exports = function math(options) {
 
   this.add('role:math,cmd:sum', function sum(msg, respond) {
-    respond(null, { answer: msg.left + msg.right })
+    respond(null, { answer: Number(msg.left).valueOf() + Number(msg.right).valueOf() })
   })
 
   this.add('role:math,cmd:product', function product(msg, respond) {
